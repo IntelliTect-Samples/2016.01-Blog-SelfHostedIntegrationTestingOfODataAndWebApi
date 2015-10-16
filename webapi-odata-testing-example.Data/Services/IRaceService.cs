653be0016706b2561b0b08d3fca92a3fd653aadd
adamskt <kelly@intellitect.com>
2015-10-16 15:56:52 -0700

    Add Race service

---
+++ webapi-odata-testing-example.Data/Services/IRaceService.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Example.Data.Models;
+
+namespace Example.Data.Services
+{
+    public interface IRaceService
+    {
+        IQueryable<Race> FindAll();
+        Task<Race> Find(int id);
+        Task<Race> Create(Race newRace);
+        Task<Race> Update(Race updated);
+        Task<int> Delete(Race delete);
+        void Dispose();
+    }
+}
---
+++ webapi-odata-testing-example.Data/Services/RaceService.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Example.Data.Models;
+
+namespace Example.Data.Services
+{
+    public class RaceService : DataServiceBase, IRaceService
+    {
+        public IQueryable<Race> FindAll()
+        {
+            return Db.Races.AsQueryable();
+        }
+
+        public async Task<Race> Find( int id )
+        {
+            return await Db.Races.FindAsync( id );
+        }
+
+        public async Task<Race> Create( Race newRace )
+        {
+            Db.Races.Add( newRace );
+            await Db.SaveChangesAsync();
+            return newRace;
+        }
+
+        public async Task<Race> Update( Race updated )
+        {
+            if ( updated == null )
+            {
+                return null;
+            }
+
+            Race existing = await Db.Races.FindAsync( updated.Id );
+            if ( existing == null )
+            {
+                return null;
+            }
+
+            Db.Entry( existing ).CurrentValues.SetValues( updated );
+            await Db.SaveChangesAsync();
+            return existing;
+        }
+
+        public async Task<int> Delete( Race delete )
+        {
+            Db.Races.Remove( delete );
+            return await Db.SaveChangesAsync();
+        }
+    }
+}
