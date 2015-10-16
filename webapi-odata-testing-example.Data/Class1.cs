b860d2c8e8e5cfa08a44f2352f55d9fc33eb249b
adamskt <kelly@intellitect.com>
2015-10-16 11:53:32 -0700

    Add Data project This is where EF migrations and data services will live.

---
+++ webapi-odata-testing-example.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webapi_odata_testing_example.Data
+{
+    public class Class1
+    {
+    }
+}
