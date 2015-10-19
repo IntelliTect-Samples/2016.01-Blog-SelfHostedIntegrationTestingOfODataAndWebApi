d4df642fe81874942341ac22b546226733db173a
adamskt <kelly@intellitect.com>
2015-10-19 10:12:56 -0700

    Finish impl of Race Controller

--- webapi-odata-testing-example/Controllers/RacesController.cs
+++ webapi-odata-testing-example/Controllers/RacesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.OData;
@@ -16,7 +17,7 @@ namespace Example.Controllers
             RaceService = raceService;
         }
 
-        private IRaceService RaceService { get; set; }
+        private IRaceService RaceService { get; }
 
         [EnableQuery]
         public IQueryable<Race> Get()
@@ -33,6 +34,59 @@ namespace Example.Controllers
                 return NotFound();
             }
             return Ok( result );
+        }
+
+        public async Task<IHttpActionResult> Post( Race race )
+        {
+            if ( ModelState.IsValid == false )
+            {
+                return BadRequest( ModelState );
+            }
+            await RaceService.Create( race );
+            if ( race.Id == 0 )
+            {
+                return BadRequest();
+            }
+            return Created( race );
+        }
+
+        public async Task<IHttpActionResult> Put( [FromODataUri] int key, Race race )
+        {
+            if ( race == null ||
+                 ModelState.IsValid == false )
+            {
+                return BadRequest();
+            }
+
+            Race original = await RaceService.Find( key );
+
+            if ( original == null )
+            {
+                return NotFound();
+            }
+
+            await RaceService.Update( race );
+
+            return Updated( race );
+        }
+
+        public async Task<IHttpActionResult> Delete( [FromODataUri] int key )
+        {
+            Race deletable = await RaceService.Find( key );
+
+            if ( deletable == null )
+            {
+                return NotFound();
+            }
+
+            int recordsDeleted = await RaceService.Delete( deletable );
+
+            if ( recordsDeleted <= 0 )
+            {
+                return BadRequest();
+            }
+
+            return StatusCode( HttpStatusCode.NoContent );
         }
     }
 }
